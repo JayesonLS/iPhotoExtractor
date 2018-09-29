f7c65550b18965b250c76b4ee7f91421eaa31fd1
Jayeson Lee-Steere <jayeson@titaniumstudios.com>
2018-09-29 11:35:56 +0800

    Fix spelling error in help.

--- iPhotoExtractor/Program.cs
+++ iPhotoExtractor/Program.cs
@@ -20,7 +20,7 @@ namespace iPhotoExtractor
 
         void PrintUsage()
         {
-            Console.WriteLine("Copys images out iPhoto library");
+            Console.WriteLine("Copies images out iPhoto library");
             Console.WriteLine("Usage: iPhotoExtractor preview|copy [options] <iPhoto library path> <dest folder path>");
             Console.WriteLine("Options: --unflaggedToExtrasFolders --copyOriginals --writeMetadataFiles --alwaysWriteMetadata --prependDateToEventNames");
         }
